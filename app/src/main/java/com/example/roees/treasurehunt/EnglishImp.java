d39c5bb015bf2657eb87ec0e365b6634df90dcbf
Roees <Vakrat@gmail.com>
2019-06-21 19:15:13 +0300

    with english translation

--- app/src/main/java/com/example/roees/treasurehunt/EnglishImp.java
+++ app/src/main/java/com/example/roees/treasurehunt/EnglishImp.java
@@ -123,101 +123,101 @@ public class EnglishImp implements LanguageImp {
 
     @Override
     public String instructorClickMapPrimary() {
-        return null;
+        return "Add new location to the game";
     }
 
     @Override
     public String instructorClickMapSecondary() {
-        return null;
+        return "By clicking the map";
     }
 
     @Override
     public String instructorEnterRiddlePrimary() {
-        return null;
+        return "";
     }
 
     @Override
     public String instructorEnterRiddleSecondary() {
-        return null;
+        return "Here you can enter the clue leading the selected location";
     }
 
     @Override
     public String instructorSendRiddlePrimary() {
-        return null;
+        return "";
     }
 
     @Override
     public String instructorSendRiddleSecondary() {
-        return null;
+        return "Save location here";
     }
 
     @Override
     public String instructorClickMarkerPrimary() {
-        return null;
+        return "You can always re-read and edit the clue";
     }
 
     @Override
     public String instructorClickMarkerSecondary() {
-        return null;
+        return "Just tap the clue marker";
     }
 
     @Override
     public String instructorDeleteMarkerPrimary() {
-        return null;
+        return "";
     }
 
     @Override
     public String instructorDeleteMarkerSecondary() {
-        return null;
+        return "While the the clue marker is selected, you can delete it by clicking the trash icon";
     }
 
     @Override
     public String instructorLocatePrimary() {
-        return null;
+        return "lost?";
     }
 
     @Override
     public String instructorLocateSecondary() {
-        return null;
+        return "Find your current location here";
     }
 
     @Override
     public String instructorStartGamePrimary() {
-        return null;
+        return "Ready to go?";
     }
 
     @Override
     public String instructorStartGameSecondary() {
-        return null;
+        return "The play button will generate a new unique code. Copy and send it to the player";
     }
 
     @Override
     public String playerRiddlePrimary() {
-        return null;
+        return "Welcome mighty treasure hunters!";
     }
 
     @Override
     public String playerRiddleSecondary() {
-        return null;
+        return "Here you can watch a clue to your next location";
     }
 
     @Override
     public String playerLocationVerifyPrimary() {
-        return null;
+        return "Got to the location?";
     }
 
     @Override
     public String playerLocationVerifySecondary() {
-        return null;
+        return "Click here to check it out!";
     }
 
     @Override
     public String playerLocatePrimary() {
-        return null;
+        return "Lost?";
     }
 
     @Override
     public String playerLocateSecondary() {
-        return null;
+        return "Find your current location here";
     }
 }
